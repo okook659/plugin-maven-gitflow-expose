c1d609d180bebc42e4ca6f26a8700db66cbc050f
Veronique04 <tekovero47@gmail.com>
2024-08-16 16:22:05 +0000

    test de la formation

--- src/main/java/mediasoft/com/App.java
+++ src/main/java/mediasoft/com/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello" );
-        System.out.println("ceci est un test");
+        System.out.println("ceci est test");
     }
 }
